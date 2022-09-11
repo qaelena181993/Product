1584946609ce62c4b10efed484d195e32d8a5ea9
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2022-09-11 22:45:27 +0300

    Initial commit

---
+++ src/main/java/ru/netology/statistic/ProductManager.java
@@ -0,0 +1,41 @@
+package ru.netology.statistic;
+
+public class ProductManager {
+    protected Repository repo;
+
+    public ProductManager(Repository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Product product) {
+        repo.save(product);
+    }
+
+    public Product[] getItems() {
+        return repo.getItems();
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repo.getItems()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
